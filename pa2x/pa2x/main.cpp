0cb59b3f95c906ea7bd7d1c0602831416d901f99
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2014-08-17 01:11:34 -0700

    solved bound check

--- pa2x/pa2x/main.cpp
+++ pa2x/pa2x/main.cpp
@@ -10,10 +10,11 @@
 #include "sorting.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    int testarray[5]={4,3,2,1,0};
-    insertion_sort(&testarray[1],4);
-    std::cout << testarray[0]<< testarray[1]<< testarray[2]<< testarray[3] << testarray[4];
+    int testarray[5]={4,1,2,3,0};
+    
+    std::cout << "1 for fail 0 for good\n"
+    << insertion_sort(&testarray[4],6) << "\n"
+    << testarray[0]<< testarray[1]<< testarray[2]<< testarray[3] << testarray[4];
 
     std::cout << "\nHello, World!\n";
     return 0;
--- sorting.cpp
+++ sorting.cpp
@@ -1,16 +1,25 @@
 // Tyler Weimin Ouyang 2014-08-15
 // cs32 pa2
 
-// Insertion Sort:
+#include <array>
+using namespace std;
 
+
+// Insertion Sort:
 int insertion_sort(int *items, const int n) {
+//    printf("size of items is %lu, and size of items[1] is %lu", sizeof items,sizeof items[1] );
+    if (n > 6) {
+        return 1;
+    }
     int temp, i, j;
     for (i=0; i<n-1; i++){
+        if (typeid(items[i]) != typeid(int())) {
+            return 1;
+        }
         if (items[i] > items[i+1]) {
             temp = items[i+1];
             for (j=i; j>=0; j--){
                 if(items[j] > temp){
-                    //int temp = item[i];
                     items[j+1]=items[j];
                     items[j]=temp;
                 }
