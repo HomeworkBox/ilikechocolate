a13fab1bc440631dbdd3e00d3b32f9649ee94730
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2014-08-16 18:47:59 -0700

    add Xcode support

---
+++ pa2x/pa2x/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pa2x
+//
+//  Created by Tyler Weimin Ouyang on 8/16/14.
+//  Copyright (c) 2014 Tyler Weimin Ouyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
