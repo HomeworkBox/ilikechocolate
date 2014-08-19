e1b119e4581c60a45e1ebeb7f4a525170d2dd7e0
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2014-08-19 14:11:08 -0700

    update

--- pa2x/pa2x/main.cpp
+++ pa2x/pa2x/main.cpp
@@ -11,19 +11,6 @@
 #include <ctime>
 #include <cstdlib>
 
-bool arrays_equal(int *a1, int *a2, const int n) {
-    if(!a1 && !a2 || n == 0)
-        return true;
-    else if(a1 && !a2 || !a1 && a2 || n < 0)
-        return false;
-    else {
-        for(int i = 0; i < n; i++) {
-            if(a1[i] != a2[i])
-                return false;
-        }
-        return 1;
-    }
-}
 
 int main(int argc, const char * argv[]) {
     int testarray[]={  -1, 0, -2 }, a2[]={ -2, -1, 0};
@@ -36,7 +23,7 @@ int main(int argc, const char * argv[]) {
 //    << testarray[0]<< testarray[1]<< testarray[2]<< testarray[3] << testarray[4];
 //
     
-    std::cout << arrays_equal(&testarray[0], &a2[0], 2)<< "\nHello, World!\n";
+    std::cout << "\nHello, World!\n";
     return 0;
 }
 
