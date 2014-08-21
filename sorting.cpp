dc178b2ede57ca249568d9b069b21c5f20470669
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2014-08-20 23:03:27 -0700

    4*10e+4 decreased to 11.9 sec

--- sorting.cpp
+++ sorting.cpp
@@ -2,7 +2,6 @@
 // cs32 pa2
 
 #include <iostream>
-#include <typeinfo>
 #include "sorting.h"
 using namespace std;
 
@@ -59,7 +58,7 @@ int merge_sort(int *items, const int n) {
 
 }
 
-int merge_sortRecursive(int *items, const int n){
+bool merge_sortRecursive(int *items, const int n){
     if (n <= 6) {
          insertion_sort(items, n);
 //        for (int z=0; z<n; z++)
@@ -103,70 +102,100 @@ int quick_sort(int *items, const int n) {
 // mergehelper
 int merge(int *items, const int n, int k){
     
-    int j=0,temp, head;
-    
     if (items[k]>= items[k-1]) {
         return 0;
     }
     else if (items[n-1] <= items [0] ) {
-        if (n%2 == 0) {
-            
-            for (j=0; j<n/2; j++,k++) {
-                temp = items[k];
-                items[k]=items[j];
-                items[j]=temp;
-            }
-        }
-        else {
-            temp=items[k-1];
-            for (j=0; j<n/2; j++,k++) {
-                items[k-1]=items[j];
-                items[j]=items[k];
-            }
-            items[n-1]=temp;
-        }
-        
+        exchange(&items[0], &items[k], &items[n-1]);
         return 0;
     }
     
     else{
         //cout << n<< '\n';
-        
-        while (j<k && k < n) {
-            if (items[j] > items[ k ]) {
-                head = k;
-                while (items[k] <= items [j] && k<n) {
-                    k++;
-                }
-                exchange(&items[j], head-j, k-head);
-                j= j- head + k;
+        int *kptr=&items[k], *jptr=&items[0];
+        while (kptr < items + n) {
+            
+            int *locationA = search(jptr, *kptr, kptr - jptr);
+            int *locationB = search(kptr, *locationA, n-k)-1;
+//            cout << '\n'<< "location a "<< locationA-items<<" ;locationB "<< locationB-items<<"\n";
+
+            if (locationB <= locationA)
+                break;
+                        exchange( locationA, kptr, locationB);
+            jptr = jptr - kptr + locationB +1;
+            k+= locationB +1 - kptr;
+            kptr = locationB+1;
+            
+//            for (int z=0; z<n; z++)
+//                cout << items[z] << " ";
+            
+        }
+//        while (jptr < kptr) {
+//            if (*jptr >= *kptr) {
+//                int temp = *kptr;
+//                for (int* i=kptr-1; i>=jptr; i--) {
+//                    *(i+1) = *i;
+//                }
+//                *jptr = temp;
 //                for (int z=0; z<n; z++)
 //                    cout << items[z] << " ";
 //                cout << '\n';
-            }
-            j++;
-        }
-        
+//            }
+//            jptr++;
+//        }
     }
-    
     
     return 0;
 }
 
-void reverse(int *items, int n)
+void reverse(int *items, int* n)
 {
-    int left = 0;
-    int right = n -1;
+    int *left = items;
+    int *right = n ;
     while(left < right)
     {
-        int temp = items[left];
-        items[left++] = items[right];
-        items[right--] = temp;
+        int temp = *left;
+        *left = *right;
+        *right = temp;
+        left++;
+        right--;
     }
 }
 
-void exchange(int *head, int left, int right){
-    reverse(head, left);
-    reverse(head+left, right);
-    reverse(head, left+right);
+void exchange(int *head, int* mid, int* right){
+    reverse(head, mid-1);
+    reverse(mid, right);
+    reverse(head, right);
+}
+
+int* search(int* head, int target, int range){
+    if (range <= 10 ) {
+        if (target<head[0])
+            return head;
+        int i;
+        for (i=0; i< range-1; i++) {
+            if (head[i] <= target && head[i+1]> target) {
+                return head+i+1;
+            }
+        }
+        return head+range;
+        
+    }
+    else if (target < head[range/2]) {
+        return search( head, target, range/2);
+    }
+    else if (target > head[range/2]){
+        if (range%2!=0)
+            return search( head+range/2+1, target, range/2);
+        else
+            return search( head+range/2+1, target, range/2-1);
+    }
+    else if (target == head[range/2]){
+        int i;
+        for ( i=range/2+1; target == head[i]; i++) ;
+        
+        return head+i;
+        
+    }
+    return head;
 }
--- sorting.h
+++ sorting.h
@@ -7,13 +7,15 @@ int insertion_sort(int *items, const int n);
 
 int merge_sort(int *items, const int n);
 
-int merge_sortRecursive(int *items, const int n);
+bool merge_sortRecursive(int *items, const int n);
 
 int merge(int *items, const int n,  int k);
 
-void reverse(int *items, int n);
+void reverse(int *items, int* n);
 
-void exchange(int *head, int left, int right);
+void exchange(int *head, int* mid, int* right);
+
+int* search(int* head, int target, int range);
 
 int quick_sort(int *items, const int n);
 
